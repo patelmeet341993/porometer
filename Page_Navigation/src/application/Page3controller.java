30fd48d5ceb2fd09c379b777d3d29ca50b02dc91
patelmeet341993 <patelmeet341993@gmail.com>
2019-07-22 10:05:24 +0530

    controler

---
+++ Page_Navigation/src/application/Page3controller.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Page3controller implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		System.out.println("I m page3");
+		
+	}
+
+}
